2927bb053ce7c806269577c8f8cbe3cb3ddb362c
Eugene Otieno <otienoeo@gmail.com>
2023-01-24 10:52:29 +0300

    Search for people

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
+import java.util.*
+
 fun main() {
-    val firstLine = readln().split(" ")
-    val secondLine = readln()
-    val  index = firstLine.indexOf(secondLine)
-    if (index == -1) println("Not Found")
-    else println(index + 1)
+    val people = getNum("Enter the number of people:")
+    println("Enter all people:")
+    val everyone: Array<String> = Array(people) { readln() }
+    println()
+    val searchNum = getNum("Enter the number of search queries:")
+
+    repeat(searchNum) {
+        println()
+        val search = getString("Enter data to search people:").trim()
+        println()
+        var found = false
+        everyone.forEach {
+            if (it.lowercase(Locale.getDefault()).contains(search.lowercase(Locale.getDefault()))) {
+                if (!found) {
+                    println("Found people:")
+                    found = true
+                }
+                println(it)
+            }
+        }
+        if (!found) println("No matching people found.")
+    }
 }
+
+fun getNum(text: String, defaultMessage: Boolean = false): Int {
+    val strErrorNum = " was not a number, please try again: "
+    var num = text
+    var default = defaultMessage
+
+    do {
+        num = getString(if (default) num + strErrorNum else num)
+        if (!default) default = true
+    } while (!isNumber(num))
+
+    return num.toInt()
+}
+
+fun getString(text: String): String {
+    println(text)
+    return readln()
+}
+
+fun isNumber(number: String) = number.toIntOrNull() != null
