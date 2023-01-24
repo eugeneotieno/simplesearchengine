121bfedcb349a068f7981107fdcfeca4cf0da427
Eugene Otieno <otienoeo@gmail.com>
2023-01-24 10:58:39 +0300

    Added menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,49 @@
 import java.util.*
 
+private lateinit var peopleList: Array<String>
+
 fun main() {
+    start()
+}
+
+fun start() {
     val people = getNum("Enter the number of people:")
     println("Enter all people:")
-    val everyone: Array<String> = Array(people) { readln() }
-    println()
-    val searchNum = getNum("Enter the number of search queries:")
+    peopleList = Array(people) { readln() }
+    menu()
+}
 
-    repeat(searchNum) {
-        println()
-        val search = getString("Enter data to search people:").trim()
-        println()
-        var found = false
-        everyone.forEach {
-            if (it.lowercase(Locale.getDefault()).contains(search.lowercase(Locale.getDefault()))) {
-                if (!found) {
-                    println("Found people:")
-                    found = true
-                }
-                println(it)
-            }
+fun menu() {
+    val menu = "\n=== Menu ===\n1. Find a person\n2. Print all people\n0. Exit"
+    var num: Int
+    var exit = false
+    do {
+        val read = getString(menu)
+        num = read.toIntOrNull() ?: getNum(read, true)
+        when (num) {
+            1 -> find()
+            2 -> printAll()
+            0 -> exit = true
+            else -> println("\nIncorrect option! Try again.")
         }
-        if (!found) println("No matching people found.")
+    } while (!exit)
+}
+
+fun find() {
+    val search = getString("\nEnter a name or email to search all suitable people.").trim()
+    var found = false
+    peopleList.forEach {
+        if (it.lowercase(Locale.getDefault()).contains(search.lowercase(Locale.getDefault()))) {
+            if (!found) found = true
+            println(it)
+        }
     }
+    if (!found) println("No matching people found.")
+}
+
+fun printAll() {
+    println("\n=== List of people ===")
+    peopleList.forEach { println(it) }
 }
 
 fun getNum(text: String, defaultMessage: Boolean = false): Int {
